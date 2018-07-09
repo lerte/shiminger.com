7cd7bb80e62e42bea33355eaaff7bfa5012f32b1
Lerte Smith <smith.shi@fantem.com>
2018-07-09 18:33:11 +0800

    try to write piano chord

---
+++ components/piano.vue
@@ -0,0 +1,119 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" width="1249" height="120">
+    <!--  52 White keys   -->
+    <rect style="fill:white;stroke:black" x="0" y="0" width="24" height="120"/>
+    <rect style="fill:white;stroke:black" x="24" y="0" width="24" height="120"/>
+    <rect style="fill:white;stroke:black" x="48" y="0" width="24" height="120"/>
+    <rect style="fill:white;stroke:black" x="72" y="0" width="24" height="120"/>
+    <rect style="fill:white;stroke:black" x="96" y="0" width="24" height="120"/>
+    <rect style="fill:white;stroke:black" x="120" y="0" width="24" height="120"/>
+    <rect style="fill:white;stroke:black" x="144" y="0" width="24" height="120"/>
+    <rect style="fill:white;stroke:black" x="168" y="0" width="24" height="120"/>
+    <rect style="fill:white;stroke:black" x="192" y="0" width="24" height="120"/>
+    <rect style="fill:white;stroke:black" x="216" y="0" width="24" height="120"/>
+    <rect style="fill:white;stroke:black" x="240" y="0" width="24" height="120"/>
+    <rect style="fill:white;stroke:black" x="264" y="0" width="24" height="120"/>
+    <rect style="fill:white;stroke:black" x="288" y="0" width="24" height="120"/>
+    <rect style="fill:white;stroke:black" x="312" y="0" width="24" height="120"/>
+    <rect style="fill:white;stroke:black" x="336" y="0" width="24" height="120"/>
+    <rect style="fill:white;stroke:black" x="360" y="0" width="24" height="120"/>
+    <rect style="fill:white;stroke:black" x="384" y="0" width="24" height="120"/>
+    <rect style="fill:white;stroke:black" x="408" y="0" width="24" height="120"/>
+    <rect style="fill:white;stroke:black" x="432" y="0" width="24" height="120"/>
+    <rect style="fill:white;stroke:black" x="456" y="0" width="24" height="120"/>
+    <rect style="fill:white;stroke:black" x="480" y="0" width="24" height="120"/>
+    <rect style="fill:white;stroke:black" x="504" y="0" width="24" height="120"/>
+    <rect style="fill:white;stroke:black" x="528" y="0" width="24" height="120"/>
+    <rect style="fill:white;stroke:black" x="552" y="0" width="24" height="120"/>
+    <rect style="fill:white;stroke:black" x="576" y="0" width="24" height="120"/>
+    <rect style="fill:white;stroke:black" x="600" y="0" width="24" height="120"/>
+    <rect style="fill:white;stroke:black" x="624" y="0" width="24" height="120"/>
+    <rect style="fill:white;stroke:black" x="648" y="0" width="24" height="120"/>
+    <rect style="fill:white;stroke:black" x="672" y="0" width="24" height="120"/>
+    <rect style="fill:white;stroke:black" x="696" y="0" width="24" height="120"/>
+    <rect style="fill:white;stroke:black" x="720" y="0" width="24" height="120"/>
+    <rect style="fill:white;stroke:black" x="744" y="0" width="24" height="120"/>
+    <rect style="fill:white;stroke:black" x="768" y="0" width="24" height="120"/>
+    <rect style="fill:white;stroke:black" x="792" y="0" width="24" height="120"/>
+    <rect style="fill:white;stroke:black" x="816" y="0" width="24" height="120"/>
+    <rect style="fill:white;stroke:black" x="840" y="0" width="24" height="120"/>
+    <rect style="fill:white;stroke:black" x="864" y="0" width="24" height="120"/>
+    <rect style="fill:white;stroke:black" x="888" y="0" width="24" height="120"/>
+    <rect style="fill:white;stroke:black" x="912" y="0" width="24" height="120"/>
+    <rect style="fill:white;stroke:black" x="936" y="0" width="24" height="120"/>
+    <rect style="fill:white;stroke:black" x="960" y="0" width="24" height="120"/>
+    <rect style="fill:white;stroke:black" x="984" y="0" width="24" height="120"/>
+    <rect style="fill:white;stroke:black" x="1008" y="0" width="24" height="120"/>
+    <rect style="fill:white;stroke:black" x="1032" y="0" width="24" height="120"/>
+    <rect style="fill:white;stroke:black" x="1056" y="0" width="24" height="120"/>
+    <rect style="fill:white;stroke:black" x="1080" y="0" width="24" height="120"/>
+    <rect style="fill:white;stroke:black" x="1104" y="0" width="24" height="120"/>
+    <rect style="fill:white;stroke:black" x="1128" y="0" width="24" height="120"/>
+    <rect style="fill:white;stroke:black" x="1152" y="0" width="24" height="120"/>
+    <rect style="fill:white;stroke:black" x="1176" y="0" width="24" height="120"/>
+    <rect style="fill:white;stroke:black" x="1200" y="0" width="24" height="120"/>
+    <rect style="fill:white;stroke:black" x="1224" y="0" width="24" height="120"/>
+    <rect style="fill:white;stroke:black" x="1248" y="0" width="24" height="120"/>
+    <!--  36 Black keys (overlap with the white keys)  -->
+    <rect style="fill:black;stroke:black" x="18" y="0" width="12" height="80"/>
+
+    <rect style="fill:black;stroke:black" x="66" y="0" width="12" height="80"/>
+    <rect style="fill:black;stroke:black" x="90" y="0" width="12" height="80"/>
+
+    <rect style="fill:black;stroke:black" x="138" y="0" width="12" height="80"/>
+    <rect style="fill:black;stroke:black" x="162" y="0" width="12" height="80"/>
+    <rect style="fill:black;stroke:black" x="186" y="0" width="12" height="80"/>
+
+    <rect style="fill:black;stroke:black" x="234" y="0" width="12" height="80"/>
+    <rect style="fill:black;stroke:black" x="258" y="0" width="12" height="80"/>
+
+    <rect style="fill:black;stroke:black" x="306" y="0" width="12" height="80"/>
+    <rect style="fill:black;stroke:black" x="330" y="0" width="12" height="80"/>
+    <rect style="fill:black;stroke:black" x="354" y="0" width="12" height="80"/>
+
+    <rect style="fill:black;stroke:black" x="402" y="0" width="12" height="80"/>
+    <rect style="fill:black;stroke:black" x="426" y="0" width="12" height="80"/>
+
+    <rect style="fill:black;stroke:black" x="474" y="0" width="12" height="80"/>
+    <rect style="fill:black;stroke:black" x="498" y="0" width="12" height="80"/>
+    <rect style="fill:black;stroke:black" x="522" y="0" width="12" height="80"/>
+
+    <rect style="fill:black;stroke:black" x="570" y="0" width="12" height="80"/>
+    <rect style="fill:black;stroke:black" x="594" y="0" width="12" height="80"/>
+
+    <rect style="fill:black;stroke:black" x="642" y="0" width="12" height="80"/>
+    <rect style="fill:black;stroke:black" x="666" y="0" width="12" height="80"/>
+    <rect style="fill:black;stroke:black" x="690" y="0" width="12" height="80"/>
+
+    <rect style="fill:black;stroke:black" x="738" y="0" width="12" height="80"/>
+    <rect style="fill:black;stroke:black" x="762" y="0" width="12" height="80"/>
+
+    <rect style="fill:black;stroke:black" x="810" y="0" width="12" height="80"/>
+    <rect style="fill:black;stroke:black" x="834" y="0" width="12" height="80"/>
+    <rect style="fill:black;stroke:black" x="858" y="0" width="12" height="80"/>
+
+    <rect style="fill:black;stroke:black" x="906" y="0" width="12" height="80"/>
+    <rect style="fill:black;stroke:black" x="930" y="0" width="12" height="80"/>
+    
+    <rect style="fill:black;stroke:black" x="978" y="0" width="12" height="80"/>
+    <rect style="fill:black;stroke:black" x="1002" y="0" width="12" height="80"/>
+    <rect style="fill:black;stroke:black" x="1026" y="0" width="12" height="80"/>
+
+    <rect style="fill:black;stroke:black" x="1074" y="0" width="12" height="80"/>
+    <rect style="fill:black;stroke:black" x="1098" y="0" width="12" height="80"/>
+    
+    <rect style="fill:black;stroke:black" x="1146" y="0" width="12" height="80"/>
+    <rect style="fill:black;stroke:black" x="1170" y="0" width="12" height="80"/>
+    <rect style="fill:black;stroke:black" x="1194" y="0" width="12" height="80"/>
+
+    <text x="3" y="110" fill="black">A2</text>
+    <text x="27" y="110" fill="black">B2</text>
+    <text x="51" y="110" fill="black">C1</text>
+    <text x="75" y="110" fill="black">D1</text>
+    <text x="99" y="110" fill="black">E1</text>
+    <text x="123" y="110" fill="black">F1</text>
+    <text x="147" y="110" fill="black">G1</text>
+    <text x="171" y="110" fill="black">A1</text>
+    <text x="195" y="110" fill="black">B1</text>
+  </svg>
+</template>
---
+++ pages/piano-chord/index.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-content>
+    <v-layout wrap>
+      <v-flex sm4 xs12 d-flex pa-4>
+        <v-select
+          :items="keyMaps"
+          label="C"
+          solo>
+        </v-select>
+      </v-flex>
+      <v-flex sm4 xs12 d-flex pa-4>
+        <v-select
+          :items="keyMaps"
+          label="E"
+          solo>
+        </v-select>
+      </v-flex>
+      <v-flex sm4 xs12 d-flex pa-4>
+        <v-select
+          :items="keyMaps"
+          label="G"
+          solo>
+        </v-select>
+      </v-flex>
+    </v-layout>
+    <v-layout text-xs-center>
+      <piano></piano>
+    </v-layout>
+  </v-content>
+</template>
+
+<script>
+import piano from '~/components/piano'
+export default {
+  components: {
+    piano
+  },
+  data: ()=> ({
+    keyMaps: [
+      'C', '#C', 
+      'bD', 'D', '#D', 
+      'bE', 'E', 
+      'F', '#F', 
+      'bG', 'G', '#G', 
+      'bA', 'A', '#A', 
+      'bB', 'B'
+    ]
+  })
+}
+</script>
