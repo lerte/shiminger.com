7e12bf0953c53e91c0a5f2b0db9ca929fd43640d
Lerte Smith <lerte@qq.com>
2018-10-18 11:59:03 +0800

    Update index.vue

--- pages/testing/index.vue
+++ pages/testing/index.vue
@@ -19,18 +19,18 @@
     layout: 'testing',
     data: ()=> ({
       screenshoots: [
-        '//cdn.shiminger.com/IMG_2719.PNG',
-        '//cdn.shiminger.com/IMG_2720.PNG',
-        '//cdn.shiminger.com/IMG_2721.PNG',
-        '//cdn.shiminger.com/IMG_2729.PNG?imageMogr2/crop/!750x480a0a0',
-        '//cdn.shiminger.com/IMG_2728.PNG',
-        '//cdn.shiminger.com/IMG_2727.PNG',
-        '//cdn.shiminger.com/IMG_2726.PNG',
-        '//cdn.shiminger.com/IMG_2722.PNG',
-        '//cdn.shiminger.com/IMG_2730.PNG',
-        '//cdn.shiminger.com/IMG_2723.PNG',
-        '//cdn.shiminger.com/IMG_2731.PNG',
-        '//cdn.shiminger.com/IMG_2725.PNG'
+        'http://cdn.shiminger.com/IMG_2719.PNG',
+        'http://cdn.shiminger.com/IMG_2720.PNG',
+        'http://cdn.shiminger.com/IMG_2721.PNG',
+        'http://cdn.shiminger.com/IMG_2729.PNG?imageMogr2/crop/!750x480a0a0',
+        'http://cdn.shiminger.com/IMG_2728.PNG',
+        'http://cdn.shiminger.com/IMG_2727.PNG',
+        'http://cdn.shiminger.com/IMG_2726.PNG',
+        'http://cdn.shiminger.com/IMG_2722.PNG',
+        'http://cdn.shiminger.com/IMG_2730.PNG',
+        'http://cdn.shiminger.com/IMG_2723.PNG',
+        'http://cdn.shiminger.com/IMG_2731.PNG',
+        'http://cdn.shiminger.com/IMG_2725.PNG'
       ]
     })
   }
